b6698c0299b2775e6d242373a9ac0bbed2dfd361
Vu Nguyen <nguyenleavu@gmail.com>
2022-11-06 13:07:36 +0700

    update chatbox

---
+++ components/FaceBook.tsx
@@ -0,0 +1,37 @@
+import Script from 'next/script';
+import React from 'react';
+
+type Props = {};
+
+const FaceBook = (props: Props) => {
+    return (
+        <>
+            <div id='fb-root'></div>
+            <Script strategy='lazyOnload' id='facebook-chat'>
+                {`
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "102521359343536");
+            chatbox.setAttribute("attribution", "biz_inbox");
+
+            window.fbAsyncInit = function() {
+            FB.init({
+             xfbml            : true,
+            version          : 'v12.0'
+             });
+            };
+
+            (function(d, s, id) {
+             var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        `}
+            </Script>
+            <div id='fb-customer-chat' className='fb-customerchat'></div>
+        </>
+    );
+};
+
+export default FaceBook;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 import Script from 'next/script';
+import FaceBook from '../components/FaceBook';
 
 const Document = () => {
     return (
@@ -21,33 +22,12 @@ const Document = () => {
                     rel='stylesheet'
                 ></link>
             </Head>
+
             <body className='dark:bg-[#151515] dark:text-[#ddd]'>
                 <Main />
                 <NextScript />
             </body>
-            <div id='fb-customer-chat' className='fb-customerchat'></div>
-            <Script strategy='lazyOnload' id='facebook-chat'>
-                {`
-                    var chatbox = document.getElementById('fb-customer-chat');
-                    chatbox.setAttribute("page_id", "102521359343536");
-                    chatbox.setAttribute("attribution", "biz_inbox");
-  
-                    window.fbAsyncInit = function() {
-                    FB.init({
-                     xfbml            : true,
-                    version          : 'v12.0'
-                     });
-                    };
-  
-                    (function(d, s, id) {
-                     var js, fjs = d.getElementsByTagName(s)[0];
-                    if (d.getElementById(id)) return;
-                    js = d.createElement(s); js.id = id;
-                    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-                    fjs.parentNode.insertBefore(js, fjs);
-                    }(document, 'script', 'facebook-jssdk'));
-                `}
-            </Script>
+            <FaceBook />
         </Html>
     );
 };
