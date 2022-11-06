d2049d51666ebbcc8fc501bc44de048c6b5283b3
Vu Nguyen <nguyenleavu@gmail.com>
2022-11-06 12:09:25 +0700

    new project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import type { AppProps } from 'next/app';
-import Layout from '../components/Layout';
+import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import '../styles/globals.css';
-import { Provider } from 'react-redux';
+import Layout from '../components/Layout';
 import { store } from '../redux/store';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps, ...appProps }: AppProps) {
     const route = ['/register', '/login', '/reset-password', '/'];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./app/**/*.{js,ts,jsx,tsx}",
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-      ],
+        './app/**/*.{js,ts,jsx,tsx}',
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
     theme: {
         typography: (theme) => ({}),
         extend: {},
     },
     darkMode: 'class',
-    plugins: [require('@tailwindcss/typography')],
-    purge: [
-        './src/pages/**/*.{js,ts,jsx,tsx}',
-        './src/components/**/*.{js,ts,jsx,tsx}',
-    ],
+    plugins: [],
 };
-
