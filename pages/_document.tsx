7b0cdca48b90b62336b89ab757b4de8ce535952b
Vu Nguyen <nguyenleavu@gmail.com>
2022-11-06 12:48:37 +0700

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 const Document = () => {
     return (
@@ -24,6 +25,29 @@ const Document = () => {
                 <Main />
                 <NextScript />
             </body>
+            <div id='fb-customer-chat' className='fb-customerchat'></div>
+            <Script strategy='lazyOnload' id='facebook-chat'>
+                {`
+                    var chatbox = document.getElementById('fb-customer-chat');
+                    chatbox.setAttribute("page_id", "102521359343536");
+                    chatbox.setAttribute("attribution", "biz_inbox");
+  
+                    window.fbAsyncInit = function() {
+                    FB.init({
+                     xfbml            : true,
+                    version          : 'v12.0'
+                     });
+                    };
+  
+                    (function(d, s, id) {
+                     var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+                    fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));
+                `}
+            </Script>
         </Html>
     );
 };
