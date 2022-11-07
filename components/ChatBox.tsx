afbc8380e36509fcb492edefbb557aff8f8a056e
Vu Nguyen <nguyenleavu@gmail.com>
2022-11-07 11:43:46 +0700

    ew

--- components/ChatBox.tsx
+++ components/ChatBox.tsx
@@ -4,33 +4,36 @@ import Script from 'next/script';
 type Props = {};
 
 function ChatBox() {
+    var chatbox: any = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute('page_id', '102521359343536');
+    chatbox.setAttribute('attribution', 'biz_inbox');
+
+    
+
+    (function (d: any, s: any, id: any) {
+        var js,
+            fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
     return (
         <div>
             <div id='fb-root'></div>
 
             <div id='fb-customer-chat' className='fb-customerchat'></div>
-            
+
             <Script strategy='lazyOnload' id='fb-customer-chat'>
                 {`
-            var chatbox = document.getElementById('fb-customer-chat');
-            chatbox.setAttribute("page_id", "102521359343536");
-            chatbox.setAttribute("attribution", "biz_inbox");
-      
-            window.fbAsyncInit = function() {
-              FB.init({
-                xfbml            : true,
-                version          : 'v12.0'
-              });
-            };
-      
-            (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-        `}
+                     window.fbAsyncInit = function() {
+                        FB.init({
+                       xfbml            : true,
+                       version          : 'v12.0'
+                       });
+                       };
+                `}
             </Script>
         </div>
     );
